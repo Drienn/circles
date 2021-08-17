bb2b17bc98b98835ead983bf0fc860b8e20e8b20
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-08-17 12:45:04 -0700

    start toggle functionality to control when the colors show

--- src/components/Circles.jsx
+++ src/components/Circles.jsx
@@ -1,27 +1,29 @@
 import './Circles.css';
+import { useState } from 'react'; 
 
-export default function Circles() {
+export default function Circles({ onToggle }) {
+  const [ color, setColor ] = useState()
 
   return (
 <div className="circle">
-  <div className="row-1">Red</div>
-  <div className="row-1">Blue</div>
-  <div className="row-1">Green</div>
-  <div className="row-1">Yellow</div>
-  <div className="row-1">Orange</div>
-  <div className="row-1">Black</div>
-  <div className="row-2">Blue</div>
-  <div className="row-2">Blue</div>
-  <div className="row-2">Blue</div>
-  <div className="row-2">Blue</div>
-  <div className="row-2">Blue</div>
-  <div className="row-2">Blue</div>
-  <div className="row-3">Red</div>
-  <div className="row-3">Green</div>
-  <div className="row-3">Red</div>
-  <div className="row-3">Green</div>
-  <div className="row-3">Red</div>
-  <div className="row-3">Green</div>
+  <div className="row red" onToggle={onToggle} value={color}><button onClick={setColor}/></div>
+  <div className="row blue" onToggle={onToggle} value={color}><button onClick={setColor}/>Blue</div>
+  <div className="row green" onToggle={onToggle} value={color}><button onClick={setColor}/>Green</div>
+  <div className="row yellow" onToggle={onToggle} value={color}><button onClick={setColor}/>Yellow</div>
+  <div className="row orange" onToggle={onToggle} value={color}><button onClick={setColor}/>Orange</div>
+  <div className="row black" onToggle={onToggle} value={color}><button onClick={setColor}/>Black</div>
+  <div className="row blue" onToggle={onToggle} value={color}><button onClick={setColor}/>Blue</div>
+  <div className="row blue" onToggle={onToggle} value={color}><button onClick={setColor}/>Blue</div>
+  <div className="row blue" onToggle={onToggle} value={color}><button onClick={setColor}/>Blue</div>
+  <div className="row blue" onToggle={onToggle} value={color}><button onClick={setColor}/>Blue</div>
+  <div className="row blue" onToggle={onToggle} value={color}><button onClick={setColor}/>Blue</div>
+  <div className="row blue" onToggle={onToggle} value={color}><button onClick={setColor}/>Blue</div>
+  <div className="row red" onToggle={onToggle} value={color}><button onClick={setColor}/></div>
+  <div className="row green" onToggle={onToggle} value={color}><button onClick={setColor}/>Green</div>
+  <div className="row red" onToggle={onToggle} value={color}><button onClick={setColor}/></div>
+  <div className="row green" onToggle={onToggle} value={color}><button onClick={setColor}/>Green</div>
+  <div className="row red" onToggle={onToggle} value={color}><button onClick={setColor}/></div>
+  <div className="row green" onToggle={onToggle} value={color}><button onClick={setColor}/>Green</div>
 </div>
   );
 }
